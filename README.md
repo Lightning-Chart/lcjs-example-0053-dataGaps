e0fe755cf16a7bb58170d80e8852569072601f12
Ilkka Kuosmanen <ilkka.kuosmanen@lightningchart.com>
2023-02-13 13:47:14 +0200

    LCJS 4.0.1

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # JavaScript Data Gaps Trading Chart
 
-![JavaScript Data Gaps Trading Chart](dataGaps.png)
+![JavaScript Data Gaps Trading Chart](dataGaps-darkGold.png)
 
 This demo application belongs to the set of examples for LightningChart JS, data visualization library for JavaScript.
 
@@ -8,13 +8,13 @@ LightningChart JS is entirely GPU accelerated and performance optimized charting
 
 The demo can be used as an example or a seed project. Local execution requires the following steps:
 
-- Make sure that relevant version of [Node.js](https://nodejs.org/en/download/) is installed
-- Open the project folder in a terminal:
+-   Make sure that relevant version of [Node.js](https://nodejs.org/en/download/) is installed
+-   Open the project folder in a terminal:
 
-        npm install              # fetches dependencies
-        npm start                # builds an application and starts the development server
+          npm install              # fetches dependencies
+          npm start                # builds an application and starts the development server
 
-- The application is available at *http://localhost:8080* in your browser, webpack-dev-server provides hot reload functionality.
+-   The application is available at _http://localhost:8080_ in your browser, webpack-dev-server provides hot reload functionality.
 
 
 ## Description
@@ -23,13 +23,13 @@ Normally, series types such as `LineSeries`, `AreaSeries`, etc. automatically co
 
 Trading data often contains frequent _data gaps_, meaning relatively large time intervals when there are no data samples available. These correspond to times when the market is "closed", evenings and weekends.
 
-[//]: # "IMPORTANT: The assets will not show before README.md is built - relative path is different!"
+[//]: # 'IMPORTANT: The assets will not show before README.md is built - relative path is different!'
 
 ![](./assets/pic1.png)
 
 In LightningChart JS, data gaps can be specified by adding an extra data point with value of `NaN` (_Not-A-Number_).
 
-[//]: # "IMPORTANT: The assets will not show before README.md is built - relative path is different!"
+[//]: # 'IMPORTANT: The assets will not show before README.md is built - relative path is different!'
 
 ![](./assets/pic2.png)
 
@@ -53,30 +53,30 @@ The code of this example shows one way of automatically detecting gaps in data s
 
 If you notice an error in the example code, please open an issue on [GitHub][0] repository of the entire example.
 
-Official [API documentation][1] can be found on [Arction][2] website.
+Official [API documentation][1] can be found on [LightningChart][2] website.
 
 If the docs and other materials do not solve your problem as well as implementation help is needed, ask on [StackOverflow][3] (tagged lightningchart).
 
-If you think you found a bug in the LightningChart JavaScript library, please contact support@arction.com.
+If you think you found a bug in the LightningChart JavaScript library, please contact support@lightningchart.com.
 
-Direct developer email support can be purchased through a [Support Plan][4] or by contacting sales@arction.com.
+Direct developer email support can be purchased through a [Support Plan][4] or by contacting sales@lightningchart.com.
 
 [0]: https://github.com/Arction/
-[1]: https://www.arction.com/lightningchart-js-api-documentation/
-[2]: https://www.arction.com
+[1]: https://lightningchart.com/lightningchart-js-api-documentation/
+[2]: https://lightningchart.com
 [3]: https://stackoverflow.com/questions/tagged/lightningchart
-[4]: https://www.arction.com/support-services/
+[4]: https://lightningchart.com/support-services/
 
-© Arction Ltd 2009-2020. All rights reserved.
+© LightningChart Ltd 2009-2022. All rights reserved.
 
 
-[Lightning chart]: https://www.arction.com/lightningchart-js-api-documentation/v3.4.0/interfaces/lightningchart.html
-[Chart XY]: https://www.arction.com/lightningchart-js-api-documentation/v3.4.0/classes/chartxy.html
-[Axis]: https://www.arction.com/lightningchart-js-api-documentation/v3.4.0/classes/axis.html
-[Dashboard]: https://www.arction.com/lightningchart-js-api-documentation/v3.4.0/classes/dashboard.html
-[Line series]: https://www.arction.com/lightningchart-js-api-documentation/v3.4.0/classes/lineseries.html
-[Area series]: https://www.arction.com/lightningchart-js-api-documentation/v3.4.0/classes/areaseriespositive.html
-[Axis tick strategies]: https://www.arction.com/lightningchart-js-api-documentation/v3.4.0/globals.html#axistickstrategies
-[Legend box]: https://www.arction.com/lightningchart-js-api-documentation/v3.4.0/classes/chartxy.html#addlegendbox
-[Zoom band chart]: https://www.arction.com/lightningchart-js-api-documentation/v3.4.0/classes/zoombandchart.html
+[Lightning chart]: https://lightningchart.com/lightningchart-js-api-documentation/v4.0.0/functions/lightningChart-1.html
+[Chart XY]: https://lightningchart.com/lightningchart-js-api-documentation/v4.0.0/classes/ChartXY.html
+[Axis]: https://lightningchart.com/lightningchart-js-api-documentation/v4.0.0/classes/Axis.html
+[Dashboard]: https://lightningchart.com/lightningchart-js-api-documentation/v4.0.0/classes/Dashboard.html
+[Line series]: https://lightningchart.com/lightningchart-js-api-documentation/v4.0.0/classes/LineSeries.html
+[Area series]: https://lightningchart.com/lightningchart-js-api-documentation/v4.0.0/classes/AreaSeriesPositive.html
+[Axis tick strategies]: https://lightningchart.com/lightningchart-js-api-documentation/v4.0.0/variables/AxisTickStrategies.html
+[Legend box]: https://lightningchart.com/lightningchart-js-api-documentation/v4.0.0/classes/Chart.html#addLegendBox
+[Zoom band chart]: https://lightningchart.com/lightningchart-js-api-documentation/v4.0.0/classes/ZoomBandChart.html
 
--- src/index.js
+++ src/index.js
@@ -2,117 +2,100 @@
  * Example showcasing how data gaps can be handled XY series. Particularly highlights line and area series in a trading use case
  */
 // Import LightningChartJS
-const lcjs = require("@arction/lcjs");
+const lcjs = require('@arction/lcjs')
 
 // Extract required parts from LightningChartJS.
-const {
-  lightningChart,
-  AxisTickStrategies,
-  emptyLine,
-  LegendBoxBuilders,
-  Themes,
-} = lcjs;
+const { lightningChart, AxisTickStrategies, emptyLine, LegendBoxBuilders, Themes } = lcjs
 
 const dashboard = lightningChart()
-  .Dashboard({
-    numberOfColumns: 1,
-    numberOfRows: 2,
-    // theme: Themes.darkGold
-  })
-  .setRowHeight(0, 1)
-  .setRowHeight(1, 0.2);
+    .Dashboard({
+        numberOfColumns: 1,
+        numberOfRows: 2,
+        // theme: Themes.darkGold
+    })
+    .setRowHeight(0, 1)
+    .setRowHeight(1, 0.2)
 
-const chart = dashboard
-  .createChartXY({ columnIndex: 0, rowIndex: 0 })
-  .setTitle("AAPL historical stock price and volume December 2021");
+const chart = dashboard.createChartXY({ columnIndex: 0, rowIndex: 0 }).setTitle('AAPL historical stock price and volume December 2021')
 
-const axisClose = chart.getDefaultAxisY().setTitle("Stock price (€)");
+const axisClose = chart.getDefaultAxisY().setTitle('Stock price (€)')
 
 const seriesClose = chart
-  .addLineSeries({
-    dataPattern: {
-      pattern: "ProgressiveX",
-    },
-  })
-  .setName("Stock price (€)")
-  .setCursorInterpolationEnabled(false)
-  .setCursorResultTableFormatter((builder, series, x, y, dataPoint) =>
-    builder
-      .addRow(series.getName())
-      .addRow(series.axisX.formatValue(dataPoint.x))
-      .addRow(`${series.axisY.formatValue(dataPoint.y)} €`)
-  );
+    .addLineSeries({
+        dataPattern: {
+            pattern: 'ProgressiveX',
+        },
+    })
+    .setName('Stock price (€)')
+    .setCursorInterpolationEnabled(false)
+    .setCursorResultTableFormatter((builder, series, x, y, dataPoint) =>
+        builder
+            .addRow(series.getName())
+            .addRow(series.axisX.formatValue(dataPoint.x))
+            .addRow(`${series.axisY.formatValue(dataPoint.y)} €`),
+    )
 
 const axisVolume = chart
-  .addAxisY({ opposite: true })
-  .setTitle("Volume (€)")
-  .setTickStrategy(AxisTickStrategies.Numeric, (ticks) =>
-    ticks
-      .setMajorTickStyle((major) => major.setGridStrokeStyle(emptyLine))
-      .setMinorTickStyle((minor) => minor.setGridStrokeStyle(emptyLine))
-  );
+    .addAxisY({ opposite: true })
+    .setTitle('Volume (€)')
+    .setTickStrategy(AxisTickStrategies.Numeric, (ticks) =>
+        ticks
+            .setMajorTickStyle((major) => major.setGridStrokeStyle(emptyLine))
+            .setMinorTickStyle((minor) => minor.setGridStrokeStyle(emptyLine)),
+    )
 
 const seriesVolume = chart
-  .addAreaSeries({ yAxis: axisVolume })
-  .setName("Volume (€)")
-  .setCursorInterpolationEnabled(false)
-  .setCursorResultTableFormatter((builder, series, position, high, low) =>
-    builder
-      .addRow(series.getName())
-      .addRow(series.axisX.formatValue(position))
-      .addRow(`${series.axisY.formatValue(high)} €`)
-  );
+    .addAreaSeries({ yAxis: axisVolume })
+    .setName('Volume (€)')
+    .setCursorInterpolationEnabled(false)
+    .setCursorResultTableFormatter((builder, series, position, high, low) =>
+        builder
+            .addRow(series.getName())
+            .addRow(series.axisX.formatValue(position))
+            .addRow(`${series.axisY.formatValue(high)} €`),
+    )
 
-const dateOrigin = new Date("2021-01-01");
-const dateOriginTime = dateOrigin.getTime();
+const dateOrigin = new Date('2021-01-01')
+const dateOriginTime = dateOrigin.getTime()
 
 const axisX = chart
-  .getDefaultAxisX()
-  .setTickStrategy(AxisTickStrategies.DateTime, (ticks) =>
-    ticks.setDateOrigin(dateOrigin)
-  )
-  // Set preset X default view.
-  .setInterval(
-    new Date("2021-12-07 20:00:00").getTime() - dateOriginTime,
-    new Date("2021-12-11 06:00:00").getTime() - dateOriginTime,
-    false,
-    true
-  );
+    .getDefaultAxisX()
+    .setTickStrategy(AxisTickStrategies.DateTime, (ticks) => ticks.setDateOrigin(dateOrigin))
+    // Set preset X default view.
+    .setInterval({
+        start: new Date('2021-12-07 20:00:00').getTime() - dateOriginTime,
+        end: new Date('2021-12-11 06:00:00').getTime() - dateOriginTime,
+    })
+const legend = chart.addLegendBox(LegendBoxBuilders.HorizontalLegendBox).add(chart)
 
-const legend = chart
-  .addLegendBox(LegendBoxBuilders.HorizontalLegendBox)
-  .add(chart);
+const zoomBandChart = dashboard.createZoomBandChart({ axis: axisX, columnIndex: 0, rowIndex: 1 }).setTitle('')
 
-const zoomBandChart = dashboard
-  .createZoomBandChart({ axis: axisX, columnIndex: 0, rowIndex: 1 })
-  .setTitle("");
-
-fetch(document.head.baseURI + "examples/assets/0053/data.json")
-  .then((r) => r.json())
-  .then((data) => {
-    // Compile XY data point lists for close series and volume series.
-    // Data gaps are programmatically detected and marked into the data set.
-    const dataGapThresholdX = 1 * 60 * 60 * 1000; // 1 hour as milliseconds
-    const closeDataXY = [];
-    const volumeDataXY = [];
-    let xPrev;
-    for (let i = 0; i < data.length; i += 1) {
-      const p = data[i];
-      if (xPrev !== undefined) {
-        const xGap = p.time - xPrev;
-        if (xGap > dataGapThresholdX) {
-          // There is a gap in the data set between data points [i - 1] and [i]
-          // Disconnect the series at this point by adding an extra data point with NaN value between these two data points.
-          closeDataXY.push({ x: p.time - dateOriginTime, y: Number.NaN });
-          volumeDataXY.push({ x: p.time - dateOriginTime, y: Number.NaN });
-          i += 1;
+fetch(document.head.baseURI + 'examples/assets/0053/data.json')
+    .then((r) => r.json())
+    .then((data) => {
+        // Compile XY data point lists for close series and volume series.
+        // Data gaps are programmatically detected and marked into the data set.
+        const dataGapThresholdX = 1 * 60 * 60 * 1000 // 1 hour as milliseconds
+        const closeDataXY = []
+        const volumeDataXY = []
+        let xPrev
+        for (let i = 0; i < data.length; i += 1) {
+            const p = data[i]
+            if (xPrev !== undefined) {
+                const xGap = p.time - xPrev
+                if (xGap > dataGapThresholdX) {
+                    // There is a gap in the data set between data points [i - 1] and [i]
+                    // Disconnect the series at this point by adding an extra data point with NaN value between these two data points.
+                    closeDataXY.push({ x: p.time - dateOriginTime, y: Number.NaN })
+                    volumeDataXY.push({ x: p.time - dateOriginTime, y: Number.NaN })
+                    i += 1
+                }
+            }
+            closeDataXY.push({ x: p.time - dateOriginTime, y: p.close })
+            volumeDataXY.push({ x: p.time - dateOriginTime, y: p.volume })
+            xPrev = p.time
         }
-      }
-      closeDataXY.push({ x: p.time - dateOriginTime, y: p.close });
-      volumeDataXY.push({ x: p.time - dateOriginTime, y: p.volume });
-      xPrev = p.time;
-    }
 
-    seriesClose.add(closeDataXY);
-    seriesVolume.add(volumeDataXY);
-  });
+        seriesClose.add(closeDataXY)
+        seriesVolume.add(volumeDataXY)
+    })
